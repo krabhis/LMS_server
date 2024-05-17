7900ba226f28fb73fdb54bc46df25ad3bb7bb727
krabhis <abhishekkr212402@gmail.com>
2024-05-18 01:30:53 +0530

    connected to db

---
+++ config/dbConnection.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.set('strictQuery', false);
+const connectionToDB=async()=>{
+  try{
+    const {connection}=await mongoose.connect(
+        process.env.MONGO_URI );
+
+    if(connection) {
+        console.log(`Connected To Mongodb: ${connection.host}`);
+    }
+
+  }
+  catch(e){
+    console.log(e);
+    process.exit(1);
+  }
+
+
+
+
+}
+
+export default connectionToDB;
+
+
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT=process.env.PORT||1000;
 
 
 app.listen(PORT,async()=>{
-
+    await connectionToDB();
     console.log(`App is running at http:localhost:-${PORT}`);     
 
 
