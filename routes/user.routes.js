30f77c3ace829880bd234e414abc813a0064173a
krabhis <abhishekkr212402@gmail.com>
2024-05-30 11:51:02 +0530

    fixes  bug

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
+import { changePassword, forgotPassword, getProfile, login, logout, register, resetPassword, updateUser } from "../controllers/user.controller.js";
+
 
 const router=Router();
 
-router.post('/register', upload.single("avatar"), register);
+router.post('/register',upload.single("avatar"),register);
 router.post('/login',login);
 router.get('/logout',logout);
-router.get('/me',  isLoggedIn, getProfile);
+router.get('/me',   getProfile);
 router.post('/forgot/password',  forgotPassword);
 router.post('/reset/:resetToken', resetPassword);
-router.post('/change-password', isLoggedIn, changePassword);
-router.put('/update', isLoggedIn, upload.single("avatar"), updateUser)
+router.post('/change-password', changePassword);
+router.put('/update' , updateUser);
+
+
+export default router;
