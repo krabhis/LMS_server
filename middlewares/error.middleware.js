bd21b9a06d9befbe47d1d3bd2cece870b06445b3
krabhis <abhishekkr212402@gmail.com>
2024-06-02 12:07:19 +0530

    fixes bug

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,14 +1,11 @@
-const errorMiddleware=(err,req,res,next)=>{
-    err.statusCode=err.statusCode=err.statusCode ||500;
-
-    err.message=err.message||"Something went wrong";
-
-    return res.status(err.statusCode).json({
-        success:false,
-        message:err.message,
-        stack:err.stack
-    });
-
-}
-
-export default errorMiddleware;
+const errorMiddleware = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || 'something went wrong';
+    
+        res.status(err.statusCode).json({
+            success: false,
+            message: err.message, 
+        }) 
+    }
+    
+    export default errorMiddleware;
