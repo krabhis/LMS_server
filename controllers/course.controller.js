2425c1ed4849df974edf0753be48f10a5019e3d3
krabhis <abhishekkr212402@gmail.com>
2024-06-04 19:11:24 +0530

    added course controllers

---
+++ controllers/course.controller.js
@@ -0,0 +1,59 @@
+import Course from '..models/course.model.js';
+import AppError from '../utils/error.util.js';
+
+
+
+
+const getAllCourses=async function (req,res,next){
+    const getAllCourses = async function(req,res,next){
+        try{
+        const courses = await Course.find({}).select('-lectures');
+    
+        res.status(200).json({
+            success:true,
+            message:'All courses',
+            courses,
+        });
+    
+    
+        }
+        catch(e){
+        return next ( new AppError(e.message,500) )
+    
+        }
+    
+    
+        }
+
+}
+
+const getLecturesByCourseId=  async function(req,res,next){
+    try{
+    const{id} = req.params;
+
+    console.log('Course Detail>', course);
+
+    const course= await Course.findById(Id);
+    console.log('Course detail>',course);
+    if(!course){
+        return next(new AppError('Invalid Course Id',400))
+    }
+    res.status(200).json({
+        success:true,
+        message:'course Lectures fetched succesfully',
+        lectures:course.lectures
+    });
+
+    }
+    catch(e){
+    return next (new AppError(e.message,500))
+
+    }
+
+
+    }
+
+export {
+    getAllCourses,
+    getLecturesByCourseId
+}
