6c3d46ce8917d0ec53071af55e007c55c223366b
krabhis <abhishekkr212402@gmail.com>
2024-06-05 19:16:38 +0530

    fixes bug

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,6 @@
 import {Router} from 'express';
-
 import {getProfile, login, logout, register, forgotPassword, resetPassword, updateUser, changePassword} from "../controllers/user.controller.js"
-import  isLoggedIn  from '../middlewares/auth.middleware.js';
+import { isLoggedIn } from '../middlewares/auth.middleware.js';
 import upload from "../middlewares/multer.middleware.js";
 
 // Braces create a destructuring pattern to extract only Router class from express module.(cocept of destructure)
@@ -14,7 +13,7 @@ router.get('/me',  isLoggedIn, getProfile);
 router.post('/forgot/password',  forgotPassword);
 router.post('/reset/:resetToken', resetPassword);
 router.post('/change-password', isLoggedIn, changePassword);
-router.put('/update/:id', isLoggedIn, upload.single("avatar"), updateUser)
+router.put('/update', isLoggedIn, upload.single("avatar"), updateUser)
 
 
 
@@ -22,6 +21,4 @@ router.put('/update/:id', isLoggedIn, upload.single("avatar"), updateUser)
 
 
 export default router;
-
-
 
