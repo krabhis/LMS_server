af6f5fe8ebf4d23d9b7f0c7a777005b6b7b9d8c8
krabhis <abhishekkr212402@gmail.com>
2024-06-05 17:45:56 +0530

    added code

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -56,6 +56,120 @@ const getLecturesByCourseId=  async function(req,res,next){
 
     }
 
+   
+    const createCourse= async (req,res,next)=>{
+        const{title, description, category, createdBy}=req.body;
+    
+        if(!title || !description||!category ||!createdBy){
+            return next(
+                new AppError('All fields are required', 400)
+            )
+    
+        }
+        const  course = await Course.create({
+            title,
+            category,
+            description,
+            createdBy
+            
+        });
+        if(!course){
+            return next(
+                new AppError('Course could not be  created , please try again',500)
+            )
+        }
+    
+        if(req.file){
+            try{
+                const result=await cloudinary.v2.uploader.upload(req.file.path,{
+                    folder:'lms'
+                });
+                console.log(JSON.stringify(result));
+                if (result){
+                    course.thumbnail.public_id=result.public_id;
+                    course.thumbnail.secure_url=result.secure_url;
+                }
+    
+                fs.rm(`uploads/${req.file.filename}`);
+            }
+            catch(e){
+                return next(
+                    new AppError(e.message,500))
+            }
+        }
+    
+        await course.save();
+    
+        res.status(200).json({
+            success:true,
+            message:'Course created successfully',
+            course,
+        })
+    
+    
+    
+    
+    
+        }
+        const updateCourse= async (req,res,next)=>{
+            try{
+                const {id}=req.params;
+                const course=await Course.findByIdAndUpdate(
+                    id,
+                    {
+                        $set:req.body
+                    },
+                    {
+                        runValidators:true
+        
+                    }
+        
+                );
+                if(!course){
+                    return next(
+                        new AppError('Course with given id does not exist',500)
+                    )
+                }
+        
+        
+                res.status(200).json({
+                    success:true,
+                    message:'Course updated successfully',
+                    course
+        
+                
+                })
+        
+            }
+        
+        
+            catch(e){
+                return next(
+                    new AppError(e.message,500))
+            }
+        
+            }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export {
     getAllCourses,
     getLecturesByCourseId
