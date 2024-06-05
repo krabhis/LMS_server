65b33e36182529763d548401a306857bd02e553d
krabhis <abhishekkr212402@gmail.com>
2024-06-05 18:28:40 +0530

    added functionalities

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -1,176 +1,265 @@
-import Course from '..models/course.model.js';
+import Course from '../models/course.model.js';
+import fs from 'fs/promises';
+import path from 'path'; 
+
+import cloudinary from 'cloudinary';
 import AppError from '../utils/error.util.js';
+const getAllCourses = async function(req,res,next){
+try{
+const courses = await Course.find({}).select('-lectures');
+
+res.status(200).json({
+    success:true,
+    message:'All courses',
+    courses,
+});
 
 
+}
+catch(e){
+return next ( new AppError(e.message,500) )
+
+}
 
 
-
-
-
-
-    const getAllCourses = async function(req,res,next){
-        try{
-        const courses = await Course.find({}).select('-lectures');
-    
-        res.status(200).json({
-            success:true,
-            message:'All courses',
-            courses,
-        });
-    
-    
-        }
-        catch(e){
-        return next ( new AppError(e.message,500) )
-    
-        }
-    
-    
-        }
-
-
+}
 
 const getLecturesByCourseId=  async function(req,res,next){
+try{
+const{id} = req.params;
+
+console.log('Course Detail>', course);
+
+const course= await Course.findById(id);
+console.log('Course detail>',course);
+if(!course){
+    return next(new AppError('Invalid Course Id',400))
+}
+res.status(200).json({
+    success:true,
+    message:'course Lectures fetched succesfully',
+    lectures:course.lectures
+});
+
+}
+catch(e){
+return next (new AppError(e.message,500))
+
+}
+
+
+}
+
+const createCourse= async (req,res,next)=>{
+const{title, description, category, createdBy}=req.body;
+
+if(!title || !description||!category ||!createdBy){
+    return next(
+        new AppError('All fields are required', 400)
+    )
+
+}
+const  course = await Course.create({
+    title,
+    category,
+    description,
+    createdBy
+    
+});
+if(!course){
+    return next(
+        new AppError('Course could not be  created , please try again',500)
+    )
+}
+
+if(req.file){
     try{
-    const{id} = req.params;
+        const result=await cloudinary.v2.uploader.upload(req.file.path,{
+            folder:'lms'
+        });
+        console.log(JSON.stringify(result));
+        if (result){
+            course.thumbnail.public_id=result.public_id;
+            course.thumbnail.secure_url=result.secure_url;
+        }
 
-    console.log('Course Detail>', course);
-
-    const course= await Course.findById(Id);
-    console.log('Course detail>',course);
-    if(!course){
-        return next(new AppError('Invalid Course Id',400))
-    }
-    res.status(200).json({
-        success:true,
-        message:'course Lectures fetched succesfully',
-        lectures:course.lectures
-    });
-
+        fs.rm(`uploads/${req.file.filename}`);
     }
     catch(e){
-    return next (new AppError(e.message,500))
-
+        return next(
+            new AppError(e.message,500))
     }
-
-
-    }
-
-   
-    const createCourse= async (req,res,next)=>{
-        const{title, description, category, createdBy}=req.body;
-    
-        if(!title || !description||!category ||!createdBy){
-            return next(
-                new AppError('All fields are required', 400)
-            )
-    
-        }
-        const  course = await Course.create({
-            title,
-            category,
-            description,
-            createdBy
-            
-        });
-        if(!course){
-            return next(
-                new AppError('Course could not be  created , please try again',500)
-            )
-        }
-    
-        if(req.file){
-            try{
-                const result=await cloudinary.v2.uploader.upload(req.file.path,{
-                    folder:'lms'
-                });
-                console.log(JSON.stringify(result));
-                if (result){
-                    course.thumbnail.public_id=result.public_id;
-                    course.thumbnail.secure_url=result.secure_url;
-                }
-    
-                fs.rm(`uploads/${req.file.filename}`);
-            }
-            catch(e){
-                return next(
-                    new AppError(e.message,500))
-            }
-        }
-    
-        await course.save();
-    
-        res.status(200).json({
-            success:true,
-            message:'Course created successfully',
-            course,
-        })
-    
-    
-    
-    
-    
-        }
-        const updateCourse= async (req,res,next)=>{
-            try{
-                const {id}=req.params;
-                const course=await Course.findByIdAndUpdate(
-                    id,
-                    {
-                        $set:req.body
-                    },
-                    {
-                        runValidators:true
-        
-                    }
-        
-                );
-                if(!course){
-                    return next(
-                        new AppError('Course with given id does not exist',500)
-                    )
-                }
-        
-        
-                res.status(200).json({
-                    success:true,
-                    message:'Course updated successfully',
-                    course
-        
-                
-                })
-        
-            }
-        
-        
-            catch(e){
-                return next(
-                    new AppError(e.message,500))
-            }
-        
-            }
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-export {
-    getAllCourses,
-    getLecturesByCourseId
 }
+
+await course.save();
+
+res.status(200).json({
+    success:true,
+    message:'Course created successfully',
+    course,
+})
+
+
+
+
+
+}
+const updateCourse= async (req,res,next)=>{
+try{
+    const {id}=req.params;
+    const course=await Course.findByIdAndUpdate(
+        id,
+        {
+            $set:req.body
+        },
+        {
+            runValidators:true
+
+        }
+
+    );
+    if(!course){
+        return next(
+            new AppError('Course with given id does not exist',500)
+        )
+    }
+
+
+    res.status(200).json({
+        success:true,
+        message:'Course updated successfully',
+        course
+
+    
+    })
+
+}
+
+
+catch(e){
+    return next(
+        new AppError(e.message,500))
+}
+
+}
+
+const removeCourse= async (req,res,next)=>{
+
+try{
+    const{id}= req.params;
+    const course=await course.findById(id);
+    if(!course){
+        return next(
+            new AppError('Course with given Id does not exist',500)
+        )
+    }
+
+    await Course.findByIdAndDelete(id);
+    res.status(200).json({
+        success:true,
+        message:'Course deleted successfully'
+    })
+
+
+
+}
+catch(e){
+    return next(
+        new AppError(e.message,500))
+}
+
+
+}
+
+const addLectureToCourseById=async(req,res,next)=>{
+try{
+    const { title,description}= req.body;
+const {id} = req.params;
+
+if(!title || !description){
+    return next(
+        new AppError('All fields are required', 400)
+    )
+
+}
+
+const course= await Course.findById(id);
+if(!course){
+    return next(
+        new AppError('Course with given id does not exist',500)
+    )
+
+}
+const lectureData={
+    title,
+    description,
+    lecture: { }
+};
+if(req.file){
+    try{
+        const result=await cloudinary.v2.uploader.upload(req.file.path,{
+            folder:'lms'
+        });
+        console.log(JSON.stringify(result));
+        if (result){
+            lectureData.lecture.public_id=result.public_id;
+            lectureData.lecture.secure_url=result.secure_url;
+        }
+
+        fs.rm(`uploads/${req.file.filename}`);
+    }
+    catch(e){
+        return next(
+            new AppError(e.message,500))
+    }
+
+
+
+
+}
+
+course.lectures.push(lectureData,500)
+course.numberOfLectures= course.lectures.length;
+
+await course.save();
+
+res.status(200).json({
+    success:true,
+    message:'Lectures successfully added to the course',
+    course
+
+});
+
+
+
+
+
+
+
+
+    
+} 
+catch(e){
+            return next(
+        new AppError(e.message,500))
+
+
+}
+
+
+}
+
+
+
+
+
+export{
+
+getAllCourses,
+getLecturesByCourseId,
+createCourse,
+updateCourse,
+removeCourse,
+addLectureToCourseById
+}
+
