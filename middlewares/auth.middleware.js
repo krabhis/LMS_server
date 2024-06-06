17002729fd0bda7640aeb241112637bc8cc057d9
krabhis <abhishekkr212402@gmail.com>
2024-06-06 23:06:36 +0530

    added admin and authorization funnality

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,16 +1,49 @@
-import AppError from "../utils/error.util.js";
-import jwt from "jsonwebtoken";
+import  AppError from "../utils/error.util.js";
+import jwt from 'jsonwebtoken';
 
 const isLoggedIn = async (req,res,next)=>{
-    const {token}=req.cookies;
+   const {token}=req.cookies;
 
-    if(!token){
-        return next(new AppError('Unauthenticated, please login again', 400))
-    }
+   if(!token){
+       return next(new AppError('Unauthenticated, please login again', 400))
+   }
 
-    const userDetails = await jwt.verify(token,process.env.JWT_SECRET);
-    req.user=userDetails;
+   const userDetails = await jwt.verify(token,process.env.JWT_SECRET);
+   req.user=userDetails;
 
-    next();
- }
- export default isLoggedIn;
+   next();
+}
+
+const authorizeRoles= (...roles)=>async(req,res,next)=>{
+  const currentUserRoles= req.user.roles;
+  if(roles.includes(currentUserRoles)){
+     return next(
+        new AppError('YOu do not  have permission to  access ')
+     )
+
+  }
+  next();
+
+}
+const authorizeSubscribers= async(req,res)=>{
+  const subscription=req.user.subscription;
+  const currentUserRoles= req.user.roles;
+
+
+  if(currentUserRoles !=='ADMIN' && subscription.status!=='active'){
+     return next(
+        new AppError('Please subscribe to access this route', 400)
+     )
+
+  }
+next();
+
+
+}
+
+export{
+   isLoggedIn,
+   authorizeRoles,
+   authorizeSubscribers
+
+}
