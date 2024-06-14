29ff016fb16a3f9272b458c28ccfe27083ec3073
krabhis <abhishekkr212402@gmail.com>
2024-06-14 14:53:46 +0530

    fixes bug

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import userRoutes from './routes/user.routes.js'
 import courseRoutes from './routes/course.routes.js'
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
+import errorMiddleware from "./middlewares/error.middleware.js"
+
 import cors from 'cors';
 import { closeSync } from 'fs';
 
@@ -12,31 +14,28 @@ import { closeSync } from 'fs';
 
 const app =express();
 
-// Routing
-app.use('/api/v1/user',userRoutes);
-app.route('/api/v1/courses',courseRoutes);
-
-
-
-
-
-
-
-
-// config();
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(cors({
-origin:[process.env.FRONTED_URL],
+origin:[process.env.FRONTEND_URL],
 credentials:true
 
 }));
 
 app.use(cookieParser());//middleware
 app.use(morgan('dev'));//middleware
+
+
+// Routing
+app.use('/api/v1/user',userRoutes);
+app.route('/api/v1/courses',courseRoutes);
+
+
+// config();
+
 
 
 // To check wheather server is up or not
@@ -46,14 +45,12 @@ res.send('/pong');
 });
 
 
-
-
 // To  any api get request then show this error
 app.all('*', (req , res)=>{
 res.status(404).send('Opps! 404 Page is not found')
 });
 
-
+app.use(errorMiddleware);
 export default app;
 
 
